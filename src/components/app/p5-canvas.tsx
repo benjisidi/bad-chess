aeeb5d78a3d0180d51dabccb279667073a3bc110
Benji Sidi <benjisidi@gmail.com>
2022-06-09 19:28:46 +0100

    Piece rendering

--- src/components/app/p5-canvas.tsx
+++ src/components/app/p5-canvas.tsx
@@ -7,11 +7,12 @@ import {isLowerCase, isUpperCase} from '@src/util';
 import {getAvailableSqs, isInCheck, movePiece} from '../../chess';
 import {useChessState} from '../atoms/chessState';
 
-const DARK_COLOR = 0;
-const LIGHT_COLOR = 196;
+const DARK_COLOR = [118, 69, 50];
+const LIGHT_COLOR = [226, 213, 188];
+const CHECK_COLOR: [number, number, number] = [200, 0, 0];
 const BOARD_SIZE = 75;
 const CANVAS_SIZE = 600;
-const AVAILABLE_SQ: [number, number, number, number] = [0, 200, 200, 200];
+const AVAILABLE_SQ: [number, number, number, number] = [31, 127, 31, 255];
 const FEN = 'rnbqkbnr/pp1ppppp/8/8/1rpPP2R/P4N2/PPP2PPP/RNBQKB1R b KQkq d3 1 2';
 
 const colMap: Map<string, number> = new Map();
@@ -32,21 +33,36 @@ const FEN2Arr = (board: string): string[][] => {
     });
     return out;
 };
-const renderPiece = (p5: p5Type, piece: string, row: number, col: number) => {
-    const color: [number, number, number] =
-        piece == piece.toUpperCase() ? [0, 102, 153] : [50, 50, 50];
-    p5.fill(...color);
-    p5.rectMode(p5.CORNERS);
-    // p5.textAlign(p5.CENTER, p5.CENTER);
-    p5.textSize(64);
-    p5.text(
-        piece,
-        col * BOARD_SIZE,
-        row * BOARD_SIZE,
-        col * BOARD_SIZE + BOARD_SIZE,
-        row * BOARD_SIZE + BOARD_SIZE,
-    );
-    p5.rectMode(p5.CORNER);
+const renderPiece = (
+    p5: p5Type,
+    piece: string,
+    row: number,
+    col: number,
+    pieceImages: Map<string, p5Type.Image>,
+) => {
+    // const color: [number, number, number] =
+    //     piece == piece.toUpperCase() ? [0, 102, 153] : [50, 50, 50];
+    // p5.fill(...color);
+    // p5.rectMode(p5.CORNERS);
+    // // p5.textAlign(p5.CENTER, p5.CENTER);
+    // p5.textSize(64);
+    // p5.text(
+    //     piece,
+    //     col * BOARD_SIZE,
+    //     row * BOARD_SIZE,
+    //     col * BOARD_SIZE + BOARD_SIZE,
+    //     row * BOARD_SIZE + BOARD_SIZE,
+    // );
+    // p5.rectMode(p5.CORNER);
+    if (pieceImages.has(piece)) {
+        p5.image(
+            pieceImages.get(piece) as p5Type.Image,
+            col * BOARD_SIZE,
+            row * BOARD_SIZE,
+            BOARD_SIZE,
+            BOARD_SIZE,
+        );
+    }
 };
 
 const parseFEN = (FEN: string): ChessState => {
@@ -81,10 +97,43 @@ const Chessboard = () => {
     const [chessState, setChessState] = useChessState();
     const [availableSqs, setAvailableSqs] = useState<number[][]>([]);
     const [checkHighlight, setCheckHighlight] = useState<number[]>([]);
+    const [pieceImages, _] = useState<Map<string, p5Type.Image>>(
+        new Map<string, p5Type.Image>(),
+    );
     const setup = (p5: p5Type, parentCanvasRef: Element) => {
         p5.createCanvas(CANVAS_SIZE, CANVAS_SIZE).parent(parentCanvasRef);
         setChessState(parseFEN(FEN));
     };
+    const preload = (p5: p5Type) => {
+        const pieces = [
+            'k',
+            'K',
+            'q',
+            'Q',
+            'b',
+            'B',
+            'n',
+            'N',
+            'r',
+            'R',
+            'p',
+            'P',
+        ];
+        pieces.forEach((piece) => {
+            if (isLowerCase(piece)) {
+                pieceImages.set(
+                    piece,
+                    p5.loadImage(`../../assets/png/black/${piece}.png`),
+                );
+            } else {
+                pieceImages.set(
+                    piece,
+                    p5.loadImage(`../../assets/png/white/${piece}.png`),
+                );
+            }
+        });
+    };
+
     const draw = (p5: p5Type) => {
         p5.background(DARK_COLOR);
         p5.fill(LIGHT_COLOR);
@@ -106,16 +155,45 @@ const Chessboard = () => {
         ) {
             availableSqs.forEach((sq) => {
                 const [x, y] = sq;
-                p5.stroke(...AVAILABLE_SQ);
-                p5.noFill();
-                p5.rect(x * BOARD_SIZE, y * BOARD_SIZE, BOARD_SIZE, BOARD_SIZE);
-                p5.noStroke();
+                // p5.noFill();
+                if (chessState.boardArray[y][x] !== '_') {
+                    p5.strokeWeight(4);
+                    p5.stroke(...AVAILABLE_SQ);
+                    p5.noFill();
+                    p5.rect(
+                        x * BOARD_SIZE,
+                        y * BOARD_SIZE,
+                        BOARD_SIZE,
+                        BOARD_SIZE,
+                    );
+                } else {
+                    p5.strokeWeight(2);
+                    p5.stroke(0);
+                    p5.fill(...AVAILABLE_SQ);
+                    p5.circle(
+                        x * BOARD_SIZE + BOARD_SIZE / 2,
+                        y * BOARD_SIZE + BOARD_SIZE / 2,
+                        BOARD_SIZE / 4,
+                    );
+                }
             });
+        }
+        if (checkHighlight.length > 0) {
+            p5.noFill();
+            p5.strokeWeight(4);
+            p5.stroke(CHECK_COLOR);
+            p5.fill(...CHECK_COLOR, 100);
+            p5.rect(
+                checkHighlight[0] * BOARD_SIZE,
+                checkHighlight[1] * BOARD_SIZE,
+                BOARD_SIZE,
+                BOARD_SIZE,
+            );
         }
         chessState.boardArray.forEach((row, i) => {
             row.forEach((piece: string, j) => {
                 if (piece !== '_') {
-                    renderPiece(p5, piece, i, j);
+                    renderPiece(p5, piece, i, j, pieceImages);
                 }
             });
         });
@@ -130,83 +208,84 @@ const Chessboard = () => {
                 BOARD_SIZE,
             );
         }
-        if (checkHighlight.length > 0) {
-            p5.noFill();
-            p5.strokeWeight(4);
-            p5.stroke(128, 0, 0);
-            p5.rect(
-                checkHighlight[0] * BOARD_SIZE,
-                checkHighlight[1] * BOARD_SIZE,
-                BOARD_SIZE,
-                BOARD_SIZE,
-            );
-        }
     };
     const mouseClicked = (e: p5Type) => {
         const x = Math.floor(e.mouseX / BOARD_SIZE);
         const y = Math.floor(e.mouseY / BOARD_SIZE);
-        console.log(chessState);
-        console.log(isInCheck(chessState.boardArray, isUpperCase, isLowerCase));
-        if (chessState.selectedX === null || chessState.selectedY === null) {
-            const whiteToMove = chessState.moveCounter % 2 == 0;
-            const selectedSq = chessState.boardArray[y][x];
+        if (x >= 0 && x < 8 && y >= 0 && y < 8) {
             if (
-                selectedSq !== '_' &&
-                ((whiteToMove && isUpperCase(selectedSq)) ||
-                    (!whiteToMove && isLowerCase(selectedSq)))
+                chessState.selectedX === null ||
+                chessState.selectedY === null
             ) {
-                setChessState({selectedX: x, selectedY: y});
-                setAvailableSqs(
-                    getAvailableSqs(
-                        chessState.boardArray,
-                        x,
-                        y,
-                        chessState.enPassant,
-                    ),
-                );
-            } else {
-                setChessState({selectedX: null, selectedY: null});
-            }
-        } else {
-            const availableSqIndex = availableSqs.map(([x, y]) => 8 * y + x);
-            const clickedSqIndex = 8 * y + x;
-            if (availableSqIndex.includes(clickedSqIndex)) {
-                const newBoard = movePiece(
-                    chessState.boardArray,
-                    chessState.selectedX,
-                    chessState.selectedY,
-                    x,
-                    y,
-                );
-                setChessState({
-                    boardArray: newBoard,
-                    selectedX: null,
-                    selectedY: null,
-                    moveCounter: chessState.moveCounter + 1,
-                });
-                const [whiteInCheck, whiteKing] = isInCheck(
-                    newBoard,
-                    isLowerCase,
-                    isUpperCase,
-                );
-                const [blackInCheck, blackKing] = isInCheck(
-                    newBoard,
-                    isUpperCase,
-                    isLowerCase,
-                );
-                if (whiteInCheck) {
-                    setCheckHighlight(whiteKing as number[]);
-                } else if (blackInCheck) {
-                    setCheckHighlight(blackKing as number[]);
+                const whiteToMove = chessState.moveCounter % 2 == 0;
+                const selectedSq = chessState.boardArray[y][x];
+                if (
+                    selectedSq !== '_' &&
+                    ((whiteToMove && isUpperCase(selectedSq)) ||
+                        (!whiteToMove && isLowerCase(selectedSq)))
+                ) {
+                    setChessState({selectedX: x, selectedY: y});
+                    setAvailableSqs(
+                        getAvailableSqs(
+                            chessState.boardArray,
+                            x,
+                            y,
+                            chessState.enPassant,
+                        ),
+                    );
                 } else {
-                    setCheckHighlight([]);
+                    setChessState({selectedX: null, selectedY: null});
                 }
             } else {
-                setChessState({selectedX: null, selectedY: null});
+                const availableSqIndex = availableSqs.map(
+                    ([x, y]) => 8 * y + x,
+                );
+                const clickedSqIndex = 8 * y + x;
+                if (availableSqIndex.includes(clickedSqIndex)) {
+                    const newBoard = movePiece(
+                        chessState.boardArray,
+                        chessState.selectedX,
+                        chessState.selectedY,
+                        x,
+                        y,
+                    );
+                    setChessState({
+                        boardArray: newBoard,
+                        selectedX: null,
+                        selectedY: null,
+                        moveCounter: chessState.moveCounter + 1,
+                    });
+                    const [whiteInCheck, whiteKing] = isInCheck(
+                        newBoard,
+                        isLowerCase,
+                        isUpperCase,
+                    );
+                    const [blackInCheck, blackKing] = isInCheck(
+                        newBoard,
+                        isUpperCase,
+                        isLowerCase,
+                    );
+                    if (whiteInCheck) {
+                        setCheckHighlight(whiteKing as number[]);
+                    } else if (blackInCheck) {
+                        setCheckHighlight(blackKing as number[]);
+                    } else {
+                        setCheckHighlight([]);
+                    }
+                } else {
+                    setChessState({selectedX: null, selectedY: null});
+                }
             }
         }
     };
-    return <Sketch setup={setup} draw={draw} mouseClicked={mouseClicked} />;
+    return (
+        <Sketch
+            setup={setup}
+            draw={draw}
+            mouseClicked={mouseClicked}
+            preload={preload}
+        />
+    );
 };
 
 export default Chessboard;
